8790b9f62547ad0d2d0aaf71a1ef3c02c3accaf4
339729139 <339729139@gotvdsb.ca>
2025-11-17 09:36:01 -0500

    DONE

---
+++ Die.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame_Topic_4___Die_Class
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+        private List<Texture2D> _faces;
+        private Rectangle _location;
+
+        public Die(List<Texture2D> faces, Rectangle location)
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+            _faces = faces;
+            _location = location;
+        }
+
+
+        public int Roll
+        {
+            get { return _roll; }
+            //set { _roll = value; }
+        }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void DrawDie(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_faces[_roll - 1], _location, Color.White);
+        }
+
+
+
+
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -9,8 +9,9 @@ namespace MonoGame_Topic_4___Die_Class
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        KeyboardState keyboardState, prevKeyboardState;
         List<Texture2D> dieTextures = new List<Texture2D>();
+        Die die1;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -21,14 +22,14 @@ namespace MonoGame_Topic_4___Die_Class
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            die1 = new Die(dieTextures, new Rectangle(10, 10, 75, 75));
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            for (int i = 0; i < 6; i++)
+            for (int i = 1; i < 7; i++)
             {
                 dieTextures.Add(Content.Load<Texture2D>($"Images/white_die_{i}"));
             }
@@ -40,7 +41,12 @@ namespace MonoGame_Topic_4___Die_Class
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            prevKeyboardState = keyboardState;
+            keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Space) && prevKeyboardState.IsKeyUp(Keys.Space))
+            {
+                die1.RollDie();
+            }
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -49,7 +55,9 @@ namespace MonoGame_Topic_4___Die_Class
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            _spriteBatch.Begin();
+            die1.DrawDie(_spriteBatch);
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
